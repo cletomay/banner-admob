e650afce658c15a8bafdfea48978eac0dc8d632f
Cleto May <cleto.may@hotmail.com>
2023-05-26 11:51:24 -0300

    Componente para desenvolvimento.

---
+++ Banner.dev.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function Banner() {
+  return (
+    <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 5}}>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default Banner;
